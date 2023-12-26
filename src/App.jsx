67a52ea7db7f585c0b66d6a33c7c422baf0af6c4
peerawet <peerawet1996@gmail.com>
2023-12-26 15:09:46 +0700

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchValue, setSearchValue] = useState("");
+  const [books, setbooks] = useState([]);
+  const getbook = async (e, searchValue) => {
+    setSearchValue(e.target.value);
+    const result = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${searchValue}`
+    );
+    setbooks(result.data.items);
+  };
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="text" onChange={(e) => getbook(e, searchValue)}></input>
+
+      {books.map((data) => {
+        return <div>{data.volumeInfo.title}</div>;
+      })}
+    </div>
+  );
 }
 
 export default App;
